4c6247ee1be43fdc49c9c45d76bfc9323ef0e9f1
dgootman <gootman@gmail.com>
2023-12-30 08:16:00 +0000

    Amazon Titan model support for Amazon Bedrock Provider

--- oa4a/amazon_bedrock_provider.py
+++ oa4a/amazon_bedrock_provider.py
@@ -1,8 +1,10 @@
 """Amazon Bedrock Provider"""
 
 import json
+import os
 import textwrap
 import uuid
+from abc import ABC, abstractmethod
 from datetime import datetime
 from typing import Generator
 
@@ -29,27 +31,34 @@ from .provider import Provider
 class AmazonBedrockProvider(Provider):
     """Amazon Bedrock Provider"""
 
-    def __init__(self) -> None:
-        self.client: BedrockRuntimeClient = boto3.client("bedrock-runtime")
+    class Engine(ABC):
+        """Bedrock engine interface for supporting different models"""
 
-    def create_chat_completion(
-        self,
-        request: CreateChatCompletionRequest,
-    ) -> (
-        CreateChatCompletionResponse
-        | Generator[CreateChatCompletionStreamResponse, None, None]
-    ):
-        """Creates a model response for the given chat conversation."""
+        @staticmethod
+        @abstractmethod
+        def body(request: CreateChatCompletionRequest) -> dict:
+            """Create Bedrock request body"""
 
-        # https://docs.aws.amazon.com/bedrock/latest/userguide/api-methods-run-inference.html
+        @staticmethod
+        @abstractmethod
+        def parse(data: dict) -> str:
+            """Parse response content from Bedrock response"""
 
-        model_id = "meta.llama2-13b-chat-v1"
+        @staticmethod
+        @abstractmethod
+        def stream_parse(data: dict) -> str:
+            """Parse response content from Bedrock response stream"""
 
-        def llama_prompt() -> str:
+    class Llama2Engine(Engine):
+        """Bedrock engine for Llama2 models"""
+
+        @staticmethod
+        def body(request: CreateChatCompletionRequest) -> dict:
             """
             Generate a Llama 2 prompt following the algorithm here:
             https://github.com/facebookresearch/llama/blob/main/llama/generation.py
             """
+
             messages = request.messages
             if messages[0].root.role == "system":
                 messages = (
@@ -73,28 +82,103 @@ class AmazonBedrockProvider(Provider):
                     + messages[2:]
                 )
 
-            return "\n\n".join(
+            prompt = "\n\n".join(
                 m.root.content.get_secret_value()
                 if m.root.role == "assistant"
                 else f"[INST]{m.root.content.get_secret_value()}[/INST]"
                 for m in messages
             )
 
-        prompt = llama_prompt()
-
-        body = json.dumps(
-            {"prompt": prompt}
-            | (
-                {"max_gen_len": request.max_tokens}
-                if request.max_tokens and request.max_tokens != "inf"
-                else {}
+            return (
+                {"prompt": prompt}
+                | (
+                    {"max_gen_len": request.max_tokens}
+                    if request.max_tokens and request.max_tokens != "inf"
+                    else {}
+                )
+                | ({"temperature": request.temperature} if request.temperature else {})
+                | ({"top_p": request.top_p} if request.top_p else {})
             )
-            | ({"temperature": request.temperature} if request.temperature else {})
-            | ({"top_p": request.top_p} if request.top_p else {})
-        )
+
+        @staticmethod
+        def parse(data: dict) -> str:
+            return data["generation"]
+
+        @staticmethod
+        def stream_parse(data: dict) -> str:
+            return AmazonBedrockProvider.Llama2Engine.parse(data)
+
+    class TitanEngine(Engine):
+        """Bedrock engine for Titan models"""
+
+        @staticmethod
+        def body(request: CreateChatCompletionRequest) -> dict:
+            """
+            Generate an Amazon Titan prompt following the algorithm here:
+            https://d2eo22ngex1n9g.cloudfront.net/Documentation/User+Guides/Titan/Amazon+Titan+Text+Prompt+Engineering+Guidelines.pdf
+            """
+            role_prefixes = {"system": "", "user": "User: ", "assistant": "Bot: "}
+
+            prompt = (
+                "\n\n".join(
+                    f"{role_prefixes[m.root.role]}{m.root.content.get_secret_value()}"
+                    for m in request.messages
+                )
+                + "\n\nBot: "
+            )
+
+            return {
+                "inputText": prompt,
+                "textGenerationConfig": {}
+                | (
+                    {"maxTokenCount": request.max_tokens}
+                    if request.max_tokens and request.max_tokens != "inf"
+                    else {}
+                )
+                | ({"temperature": request.temperature} if request.temperature else {})
+                | ({"topP": request.top_p} if request.top_p else {}),
+            }
+
+        @staticmethod
+        def parse(data: dict) -> str:
+            return "\n".join(result["outputText"] for result in data["results"])
+
+        @staticmethod
+        def stream_parse(data: dict) -> str:
+            return data["outputText"]
+
+    ENGINES: dict[str, Engine] = {
+        "meta.llama2-13b-chat-v1": Llama2Engine(),
+        "amazon.titan-text-express-v1": TitanEngine(),
+    }
+
+    def __init__(
+        self,
+        bedrock_model=os.environ.get("BEDROCK_MODEL", "amazon.titan-text-express-v1"),
+    ) -> None:
+        self.client: BedrockRuntimeClient = boto3.client("bedrock-runtime")
+        self.bedrock_model = bedrock_model
+
+    def create_chat_completion(
+        self,
+        request: CreateChatCompletionRequest,
+    ) -> (
+        CreateChatCompletionResponse
+        | Generator[CreateChatCompletionStreamResponse, None, None]
+    ):
+        """Creates a model response for the given chat conversation."""
+
+        # https://docs.aws.amazon.com/bedrock/latest/userguide/api-methods-run-inference.html
+
+        model_id = self.bedrock_model
+
+        engine = AmazonBedrockProvider.ENGINES[model_id]
+
+        body = json.dumps(engine.body(request))
 
         logger.trace(f"bedrock request: {dict({'body': body, 'modelId': model_id})}")
 
+        # pylint: disable=duplicate-code
         # pylint: disable-next=no-else-return
         if request.stream:
             response = self.client.invoke_model_with_response_stream(
@@ -108,16 +192,17 @@ class AmazonBedrockProvider(Provider):
             def stream_response():
                 for event in stream:
                     chunk = event["chunk"]
-                    line = chunk["bytes"].decode()
-                    logger.trace(f"bedrock response stream: {line}")
-                    data = json.loads(line)
+                    body = chunk["bytes"].decode()
+                    logger.trace(f"bedrock response stream: {body}")
+                    data = json.loads(body)
+                    content = engine.stream_parse(data)
 
                     yield CreateChatCompletionStreamResponse(
                         id=response_id,
                         choices=[
                             Choice3(
                                 delta=ChatCompletionStreamResponseDelta(
-                                    content=SecretStr(data["generation"]),
+                                    content=SecretStr(content),
                                     role="assistant",
                                 ),
                                 finish_reason=None,
@@ -136,17 +221,17 @@ class AmazonBedrockProvider(Provider):
             response = self.client.invoke_model(
                 body=body, modelId=model_id, accept=accept, contentType=content_type
             )
-            response_body = json.loads(response["body"].read())
-            logger.trace(f"bedrock response: {response_body}")
-
-            generation = response_body["generation"]
+            body = response["body"].read().decode()
+            logger.trace(f"bedrock response: {body}")
+            data = json.loads(body)
+            content = engine.parse(data)
 
             response = CreateChatCompletionResponse(
                 id=str(uuid.uuid4()),
                 choices=[
                     Choice1(
                         message=ChatCompletionResponseMessage(
-                            content=SecretStr(generation),
+                            content=SecretStr(content),
                             role="assistant",
                         ),
                         finish_reason="stop",
